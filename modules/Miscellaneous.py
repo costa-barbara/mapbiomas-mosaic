cec0032855b09c50cf48d1b41df3aa9654dbb777
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2025-05-21 15:42:06 -0300

    Update Miscellaneous.py

--- modules/Miscellaneous.py
+++ modules/Miscellaneous.py
@@ -1,30 +1,47 @@
 import ee
+import math
 #
 #
 #
+# Calculates terrain slope (in %) from the MERIT DEM and adds it as a new band to the input image
 def getSlope(image):
+    """
+    Adds a slope band (percent) to the input image, derived from the MERIT DEM.
 
-    terrain = ee.Image("JAXA/ALOS/AW3D30_V1_1").select("AVE")
-
-    slope = ee.Terrain.slope(terrain)\
-        .multiply(100)\
-        .int16()\
-        .rename('slope')
-
-    return image.addBands(slope)
-
-#
-#
-#
-def getEntropyG(image):
-
-    """Calculate textG
-    
     Parameters:
-        image (ee.Image): image containing the Green band:
-        
+        image (ee.Image): The image to which the slope band will be added.
+
     Returns:
-        ee.Image:  image with textG band
+        ee.Image: Input image with an additional 'slope' band in percent.
+    """
+    
+    # Load MERIT DEM and compute slope in degrees
+    terrain = ee.Image("MERIT/DEM/v1_0_3").select('dem')
+    slope_deg = ee.Terrain.slope(terrain)
+    
+    # Convert slope from degrees to percent: tan(deg * pi / 180) * 100
+    slope_pct = slope_deg.expression(
+      'tan (pi/180 * deg) * 100', {
+        'deg': slope_deg,
+        'pi': ee.Number(math.pi)
+      }).rename('slope').toInt16()
+
+    return image.addBands(slope_pct)
+
+#
+#
+#
+# Computes entropy texture on the green band to assess spatial variation in vegetation or brightness
+def getEntropyG(image):
+    """
+    Computes entropy texture on the green band using a 5-pixel square kernel.
+    Intended for use with Landsat monthly mosaics that include 'green_median'.
+
+    Parameters:
+        image (ee.Image): Image containing the 'green_median' band.
+
+    Returns:
+        ee.Image: Input image with an additional 'green_median_texture' band (0–100).
     """
     square = ee.Kernel.square(radius=5)
 
