12f65d4d1ba5c1f45b34a027d5588707eefbe7d9
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2025-06-02 11:14:37 -0300

    Update ThreeYearMetrics.py

--- modules/ThreeYearMetrics.py
+++ modules/ThreeYearMetrics.py
@@ -1,6 +1,6 @@
 import ee
 
-def addThreeYearMetrics(year, mosaic, mosaic_dict, aoi_mask):
+def addThreeYearMetrics(year, mosaic, mosaic_dict):
     """
     Adds three-year metrics related to NDVI and NBR to the given mosaic.
 
@@ -8,7 +8,6 @@ def addThreeYearMetrics(year, mosaic, mosaic_dict, aoi_mask):
         year (int): The target year to compute metrics for.
         mosaic (ee.Image): The current year's mosaic.
         mosaic_dict (dict): Dictionary of mosaics for all years {year: ee.Image}.
-        aoi_mask (ee.Image): Binary mask (1 = valid pixel, 0 = masked) for the analysis area.
 
     Returns:
         ee.Image: Mosaic with added bands:
@@ -19,9 +18,9 @@ def addThreeYearMetrics(year, mosaic, mosaic_dict, aoi_mask):
 
     if year not in mosaic_dict or (year - 1) not in mosaic_dict or (year - 2) not in mosaic_dict:
         # If there are not enough previous years to compute 3-year metrics
-        amp_ndvi_3yr = ee.Image(0).rename('amp_ndvi_3yr').updateMask(aoi_mask.eq(1))
-        var_ndvi_p25_3yr = ee.Image(0).rename('var_ndvi_p25_3yr').updateMask(aoi_mask.eq(1))
-        var_nbr_median_3yr = ee.Image(0).rename('var_nbr_median_3yr').updateMask(aoi_mask.eq(1))
+        amp_ndvi_3yr = ee.Image(0).rename('amp_ndvi_3yr')
+        var_ndvi_p25_3yr = ee.Image(0).rename('var_ndvi_p25_3yr')
+        var_nbr_median_3yr = ee.Image(0).rename('var_nbr_median_3yr')
     else:
         # Get the mosaics from the current and two previous years
         mosaics_3yr = [
@@ -34,24 +33,21 @@ def addThreeYearMetrics(year, mosaic, mosaic_dict, aoi_mask):
         min_ndvi_dry = ee.ImageCollection.fromImages([m.select('ndvi_median_dry').toFloat() for m in mosaics_3yr]).min()
         max_ndvi_wet = ee.ImageCollection.fromImages([m.select('ndvi_median_wet').toFloat() for m in mosaics_3yr]).max()
         amp_ndvi_3yr = max_ndvi_wet.subtract(min_ndvi_dry)\
-            .rename('amp_ndvi_3yr')\
-            .updateMask(aoi_mask.eq(1))
+            .rename('amp_ndvi_3yr')
 
         # Compute variance of NDVI P25 over 3 years
         ndvi_p25_stack = ee.ImageCollection.fromImages([m.select('ndvi_p25') for m in mosaics_3yr])
         mean_p25 = ndvi_p25_stack.reduce(ee.Reducer.mean())
         var_ndvi_p25_3yr = ndvi_p25_stack.map(lambda img: img.subtract(mean_p25).pow(2))\
             .reduce(ee.Reducer.mean())\
-            .rename('var_ndvi_p25_3yr')\
-            .updateMask(aoi_mask.eq(1))
+            .rename('var_ndvi_p25_3yr')
 
         # Compute variance of NBR median over 3 years
         nbr_stack = ee.ImageCollection.fromImages([m.select('nbr_median') for m in mosaics_3yr])
         mean_nbr = nbr_stack.reduce(ee.Reducer.mean())
         var_nbr_median_3yr = nbr_stack.map(lambda img: img.subtract(mean_nbr).pow(2))\
             .reduce(ee.Reducer.mean())\
-            .rename('var_nbr_median_3yr')\
-            .updateMask(aoi_mask.eq(1))
+            .rename('var_nbr_median_3yr')
 
     # Add all metrics to the original mosaic
     return mosaic.addBands(amp_ndvi_3yr)\
