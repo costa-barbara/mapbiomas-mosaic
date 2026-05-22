f5afc4d759b97f0c57fae4df2c6c9a7af42bf6a7
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2026-05-22 17:57:54 -0300

    Update SpectralIndexes.py

--- collection_11/version_01/SpectralIndexes.py
+++ collection_11/version_01/SpectralIndexes.py
@@ -1,6 +1,7 @@
 #
 import ee
 
+
 def getNDVI(image):
 
     exp = '( b("nir") - b("red") ) / ( b("nir") + b("red") )'
@@ -114,7 +115,7 @@ def getGNDVI(image):
 def getMSAVI(image):
 
     exp = '(2 * b("nir") + 1 - sqrt((2 * b("nir") + 1) ** 2 - 8 * (b("nir") - b("red")))) / 2'
-    
+
     msavi = image.expression(exp)\
         .rename(["msavi"])\
         .add(1)
@@ -125,7 +126,7 @@ def getMSAVI(image):
 def getNBR(image):
 
     exp = '( b("nir") - b("swir2") ) / ( b("nir") + b("swir2") )'
-    
+
     nbr = image.expression(exp)\
         .rename(["nbr"])\
         .add(1)
@@ -165,15 +166,16 @@ def getTGSI(image):
         .add(1)
 
     return image.addBands(tgsi)
-    
+
+
 #--------------------------------------
 # Specific indexes for Sentinel-2 data
 #--------------------------------------
 
-def getNDVIRED (image):
-     """
-     Normalized difference vegetation index with red edge band (NDVI Red)
-     """
+def getNDVIRED(image):
+    """
+    Normalized difference vegetation index with red edge band (NDVI Red)
+    """
     exp = '( b("red_edge_1") - b("red") ) / ( b("red_edge_1") + b("red") )'
 
     ndviRed = image.expression(exp)\
@@ -184,10 +186,10 @@ def getNDVIRED (image):
     return image.addBands(ndviRed, overwrite=True)
 
 
-def getVI700 (image):
-     """
-     Normalized Difference Chlorophyll Index (NDCI)
-     """
+def getVI700(image):
+    """
+    Normalized Difference Chlorophyll Index (NDCI)
+    """
     exp = '( b("red_edge_1") - b("red_edge_2") ) / ( b("red_edge_1") + b("red_edge_2") )'
 
     ndci = image.expression(exp)\
@@ -198,10 +200,10 @@ def getVI700 (image):
     return image.addBands(ndci, overwrite=True)
 
 
-def getIRECI (image):
-     """
-     Inverted red-edge chlorophyll index (IRECI)
-     """
+def getIRECI(image):
+    """
+    Inverted red-edge chlorophyll index (IRECI)
+    """
     exp = '( b("red_edge_3") - b("red") ) / ( b("red_edge_1") + b("red_edge_2") )'
 
     ireci = image.expression(exp)\
@@ -212,10 +214,10 @@ def getIRECI (image):
     return image.addBands(ireci, overwrite=True)
 
 
-def getCIRE (image):
-     """
-     Chlorophyll index red edge (CIRE)
-     """
+def getCIRE(image):
+    """
+    Chlorophyll index red edge (CIRE)
+    """
     exp = '( b("nir") - b("red_edge_1") )'
 
     cire = image.expression(exp)\
@@ -226,10 +228,10 @@ def getCIRE (image):
     return image.addBands(cire, overwrite=True)
 
 
-def getTCARI (image):
-     """
-     Transformed chlorophyll absorption in reflectance index (TCARI)
-     """
+def getTCARI(image):
+    """
+    Transformed chlorophyll absorption in reflectance index (TCARI)
+    """
     exp = '( (b("red_edge_1") - b("red") ) - 0.2 * ( b("red_edge_1") - b("green") ) ) * ( b("red_edge_1") / b("red") ) * 3'
 
     tcari = image.expression(exp)\
@@ -240,10 +242,10 @@ def getTCARI (image):
     return image.addBands(tcari, overwrite=True)
 
 
-def getSFDVI (image):
-     """
-     Spectral feature depth vegetation index (SFDVI)
-     """
+def getSFDVI(image):
+    """
+    Spectral feature depth vegetation index (SFDVI)
+    """
     exp = '( (( b("green") + b("nir") ) / 2 ) - ( b("red") + b("red_edge_1") ) / 2 )'
 
     sfdvi = image.expression(exp)\
@@ -254,10 +256,10 @@ def getSFDVI (image):
     return image.addBands(sfdvi, overwrite=True)
 
 
-def getNDRE (image):
-     """
-     Normalized difference red edge index
-     """
+def getNDRE(image):
+    """
+    Normalized difference red edge index
+    """
     exp = '( b("nir") - b("red_edge_1")) / ( b("nir") + b("red_edge_1")) '
 
     ndre = image.expression(exp)\
@@ -265,4 +267,4 @@ def getNDRE (image):
         .rename(["ndre"])\
         .add(1)
 
-      return image.addBands(ndre, overwrite=True)
+    return image.addBands(ndre, overwrite=True)
